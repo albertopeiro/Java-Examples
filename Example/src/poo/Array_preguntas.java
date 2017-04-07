2727639d6d756fe10fc82feb21afacc2cd9d1d2a
albertopeiro <albpeiro19@gmail.com>
2017-04-07 14:25:15 +0100

    Add files via upload

--- Example/src/poo/Array_preguntas.java
+++ Example/src/poo/Array_preguntas.java
@@ -5,18 +5,18 @@ public class Array_preguntas {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String n=JOptionPane.showInputDialog("Introduce el número de preguntas");
-		int m=Integer.parseInt(n);
-		String [] paises=new String[m];
+
+		
+		String [] paises=new String[17];
 		
 		for(int i=0; i<paises.length; i++){
 		paises[i]=JOptionPane.showInputDialog("Introduce la pregunta " + (i+1));
 		}
-		
 	String seguimos ="";
 		while (seguimos.equalsIgnoreCase("no")==false){
-		int j=(int) Math.round(Math.random()*(paises.length-1));
-		seguimos=JOptionPane.showInputDialog(j+1 +" " + paises[j] + ". Seguimos? Introduce 'no' para salir");
+		int j=(int) Math.random()*paises.length;
+		seguimos=JOptionPane.showInputDialog(paises[j]+ "Seguimos?");
+		
 		}
 	
 	}
--- Example/src/poo/PruebaTemporizador2.java
+++ Example/src/poo/PruebaTemporizador2.java
@@ -12,10 +12,9 @@ public class PruebaTemporizador2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//Instancia de la clase reloj
-		Reloj mireloj=new Reloj();
-//Inicio método en marcha
-		mireloj.enMarcha(3000,true);
+
+		Reloj mireloj=new Reloj(3000,true);
+		mireloj.enMarcha();
 		
 		JOptionPane.showMessageDialog(null, "Pulsa Aceptar para terminar");
 		
@@ -26,48 +25,28 @@ public class PruebaTemporizador2 {
 
 class Reloj{
 	
-	public Reloj (){
-		// Estos campos de clase ya no son necesarios, estan en la clase interna local
-		//this.intervalo=intervalo;
-		//this.sonido=sonido;
+	public Reloj (int intervalo, boolean sonido){
+		
+		this.intervalo=intervalo;
+		this.sonido=sonido;
 		//This para los campos de clase, o sea las variables declaradas dentro de la clase
 		//Y los no this son los pertenecientes al método constructo public Reloj
 		
 	}
 	
-	public void enMarcha(int intervalo, final boolean sonido){	
-			//Clase interna Local. Si necesita parámetros de entrada a los que la clase interna tenga que acceder,
-			// hay que poner la palabra reservada final, como es el caso de sonido. La variable intervalo no es
-			// necesaria dentro de la clase interna, así que no es necesario poner final
-			class DameLaHora2 implements ActionListener{
-			
-			public void actionPerformed(ActionEvent evento){
-				
-				Date ahora=new Date();
-				System.out.println("Te pongo la hora cada 3 segundos: " + ahora); 
-			
-					if(sonido==true){
-					
-					Toolkit.getDefaultToolkit().beep();
-					}
-				}
-			
-			}
-			//Crea un objeto oyente de ActionListener		
-			ActionListener oyente=new DameLaHora2();
-			//Nueva clase pero de tipo interfaz ActionListener
-			Timer mitemporizador2=new Timer(3000, oyente);
-			//Instancia de la clase timer
-			mitemporizador2.start();
+	public void enMarcha(){
+		
+		ActionListener oyente=new DameLaHora2();
+		//Nueva clase pero de tipo interfaz ActionListener
+		Timer mitemporizador2=new Timer(3000, oyente);
+		//Instancia de la clase timer
+		mitemporizador2.start();
 	}
 	
-	
-	// No hacen falta ahora estos campos de clase
-	//private int intervalo;
-	//private boolean sonido;
+	private int intervalo;
+	private boolean sonido;
 
-	//Clase interna a la clase reloj
-	/*private class DameLaHora2 implements ActionListener{
+	private class DameLaHora2 implements ActionListener{
 		
 		public void actionPerformed(ActionEvent evento){
 			
@@ -80,7 +59,7 @@ class Reloj{
 			}
 		}
 		
-	} */
+	}
 	
 	
 }
